e3bff3bb377e4aa5e836992f890e8af1f7744009
Nova Aurata <545406+qwiddity@users.noreply.github.com>
2023-09-04 18:42:52 +1000

    Quartz sync: Sep 4, 2023, 6:42 PM

--- content/index.md
+++ content/index.md
@@ -4,7 +4,7 @@ title: Egopedia
 # Heading
 This is a new-ish Quartz installation for Egopedia
 
-[[Untitled]]
+[[SuperEgo]]
 [[Untitled 1]]
 [[Untitled 2]]
 [[Untitled 3]]
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz Egopedia",
+    pageTitle: "🪴 Egopedia",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
